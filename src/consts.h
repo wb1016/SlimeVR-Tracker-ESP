339f2693d422f754888dab9f256cc14829750176
blueon <star_tracker@protonmail.com>
2025-12-19 13:45:09 +0900

    fix missing consts and wrong defines

--- src/consts.h
+++ src/consts.h
@@ -96,6 +96,8 @@ enum class SensorTypeID : uint8_t {
 #define BOARD_GESTURES 21  // Used by Gestures
 #define BOARD_SLIMEVR_V1_2 22  // SlimeVR v1.2
 #define BOARD_ESP32S3_SUPERMINI 23
+#define BOARD_NARELR 24
+#define BOARD_NAREB1 25
 #define BOARD_DEV_RESERVED 250  // Reserved, should not be used in any release firmware
 
 #define BAT_EXTERNAL 1
